b5d229537e1592a2ad1293f406fc9ade6a305d3e
Twan Goosen <twan@clarin.eu>
2017-09-14 14:04:18 +0200

    Solr configuration and settings

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
+
 PROJECT_NAME=vlo-solr
 VERSION=1.0
+
+VLO_DISTRIBUTION_PACKAGE="https://github.com/clarin-eric/VLO/releases/download/vlo-4.2.1/vlo-4.2.1-Distribution.tar.gz"
+VLO_SOLR_CONF_DIR="config/solr/collection1/conf"
 
 REV=$(git rev-parse --short HEAD)
 TAG=1.0-SNAPSHOT-${REV:-latest}
@@ -8,8 +13,25 @@ IMAGE_QUALIFIED_NAME="$PROJECT_NAME:${TAG}"
 
 BASEDIR=$(dirname "$0")
 IMAGE_DIR="${BASEDIR}/image"
+IMAGE_TMP_DIR="${IMAGE_DIR}/tmp"
+SOLR_CONF_TARGET_DIR="${IMAGE_TMP_DIR}/solr-conf"
 
-# TODO: prepare config
+VLO_TMP_DIR="${TMPDIR}/vlo-$(date +'%s')"
+
+if [ -d "${IMAGE_TMP_DIR}" ]
+then
+	rm -rf "${IMAGE_TMP_DIR}"
+fi
+mkdir -p "${IMAGE_TMP_DIR}"
+mkdir -p "${VLO_TMP_DIR}"
+
+# Retrieve VLO distribution
+echo "Retrieving and unpacking VLO distribution..."
+(cd ${VLO_TMP_DIR} &&
+	curl -L\# ${VLO_DISTRIBUTION_PACKAGE}| tar zx --strip-components=1)
+
+# Prepare config
+cp -R "${VLO_TMP_DIR}/${VLO_SOLR_CONF_DIR}" "${SOLR_CONF_TARGET_DIR}"
 
 # Build image
 echo "Building ${IMAGE_QUALIFIED_NAME}"
@@ -25,3 +47,6 @@ echo -e "\n\nDone! To start, run the following command:
 Then visit:
 
 	http://localhost:8983/solr/"
+
+rm -rf ${IMAGE_TMP_DIR}
+rm -rf ${VLO_TMP_DIR}
--- image/docker-entrypoint-initdb.d/configure-solr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /opt/solr/server/solr/vlo/conf/
-sed -i -e 's_</config>_<schemaFactory class="ClassicIndexSchemaFactory"/>\n</config>_g' solrconfig.xml
