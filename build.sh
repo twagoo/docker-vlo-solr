dc43e8bfd14103124b017314a0c4d5fe086adf5b
Twan Goosen <twan@clarin.eu>
2017-09-28 14:51:05 +0200

    "Local" settings from build script moved to env.sh

--- build.sh
+++ build.sh
@@ -4,21 +4,9 @@ set -e
 PROJECT_NAME=vlo-solr
 REV=$(git rev-parse --short HEAD)
 TAG=1.0-SNAPSHOT-${REV:-latest}
-
-VLO_DISTRIBUTION_PACKAGE="https://github.com/clarin-eric/VLO/releases/download/vlo-4.2.1/vlo-4.2.1-Distribution.tar.gz"
-SOLR_CONF_PACKAGE="https://github.com/clarin-eric/VLO/archive/issue87.tar.gz"
-SOLR_CONF_DIR="vlo-web-app/src/test/resources/solr/collection1"
-
-IMAGE_QUALIFIED_NAME="$PROJECT_NAME:${TAG}"
-IMAGE_QUALIFIED_NAME_WITH_DATA="${PROJECT_NAME}-data:${TAG}"
-
 BASEDIR=$(dirname "$0")
-IMAGE_DIR="${BASEDIR}/image"
-IMAGE_TMP_DIR="${IMAGE_DIR}/tmp"
-SOLR_CONF_TARGET_DIR="${IMAGE_TMP_DIR}/solr-conf"
 
-VLO_TMP_DIR="${TMPDIR}/vlo-$(date +'%s')"
-SOLR_CONF_TMP_DIR="${TMPDIR}/vlo-solr-$(date +'%s')"
+source ${BASEDIR}/script/env.sh
 
 if [ -z "${DATAROOT_DIR}" ]
 then
@@ -70,35 +58,8 @@ echo "Building ${IMAGE_QUALIFIED_NAME}"
 	
 # Run import and commit
 if [ "${IMPORT}" -eq 1 ]; then
-	echo "Adapting VLO importer configuration"
-	# Configure to connect to instance
-	sed -e 's_<solrUrl>.*</solrUrl>_<solrUrl>http://localhost:8983/solr/collection1/</solrUrl>_g' ${VLO_TMP_DIR}/config/VloConfig.xml > ${VLO_TMP_DIR}/config/VloConfig-docker.xml
-	
-	# Prepare data roots
-	sed -e "s@__DATAROOT_DIR__@${DATAROOT_DIR}@g" ${BASEDIR}/vlo-config/dataroots-docker.xml > ${VLO_TMP_DIR}/config/dataroots-docker.xml
-	sed -i -e 's_<xi:include href="dataroots-production.xml"_<xi:include href="dataroots-docker.xml"_g' ${VLO_TMP_DIR}/config/VloConfig-docker.xml
-	
-	# Use built-in mapping files (remove file path prefix)
-	sed -i -e 's_file:/srv/VLO-mapping__g' ${VLO_TMP_DIR}/config/VloConfig-docker.xml
-	
-	echo "Starting Solr container"
-	docker run --name vlo_solr_import -d -p 8983:8983 -t ${IMAGE_QUALIFIED_NAME}
-	# Wait for Solr to come online
-	sleep 5
-	while ! curl -s http://localhost:8983/solr
-	do
-		echo Waiting...
-		sleep 5
-	done
-	sleep 5
-	echo "Running importer"
-	(cd ${VLO_TMP_DIR}/bin && ./vlo_solr_importer.sh -c ${VLO_TMP_DIR}/config/VloConfig-docker.xml)
-	cp ${VLO_TMP_DIR}/log/*.log .
-	echo "Stopping Solr container"
-	docker stop vlo_solr_import
-	echo "Committing to ${IMAGE_QUALIFIED_NAME_WITH_DATA}" 
-	docker commit vlo_solr_import ${IMAGE_QUALIFIED_NAME_WITH_DATA}
-	docker rm vlo_solr_import
+	export BASEDIR VLO_TMP_DIR DATAROOT_DIR IMAGE_QUALIFIED_NAME IMAGE_QUALIFIED_NAME_WITH_DATA
+	${BASEDIR}/script/import-into-container.sh
 fi
 
 echo -e "\n\nDone! To start, run the following command: 
---
+++ script/env.sh
@@ -0,0 +1,13 @@
+VLO_DISTRIBUTION_PACKAGE="https://github.com/clarin-eric/VLO/releases/download/vlo-4.2.1/vlo-4.2.1-Distribution.tar.gz"
+SOLR_CONF_PACKAGE="https://github.com/clarin-eric/VLO/archive/issue87.tar.gz"
+SOLR_CONF_DIR="vlo-web-app/src/test/resources/solr/collection1"
+
+IMAGE_QUALIFIED_NAME="$PROJECT_NAME:${TAG}"
+IMAGE_QUALIFIED_NAME_WITH_DATA="${PROJECT_NAME}-data:${TAG}"
+
+IMAGE_DIR="${BASEDIR}/image"
+IMAGE_TMP_DIR="${IMAGE_DIR}/tmp"
+SOLR_CONF_TARGET_DIR="${IMAGE_TMP_DIR}/solr-conf"
+
+VLO_TMP_DIR="${TMPDIR}/vlo-$(date +'%s')"
+SOLR_CONF_TMP_DIR="${TMPDIR}/vlo-solr-$(date +'%s')"
